81a31f8bd1c2e7d3c212aced243e67561a8caff6
Shusanto Kumar Modak <shusanto294@gmail.com>
2022-09-03 01:32:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue
+    
+createApp({
+  data() {
+    return {
+      calculation: 0,
+      calculationString: '',
+      calculationStringOnScreen: ''
+    }
+  },
+  methods: {
+      updateCanlucationString(input){
+          this.calculationString += input;
+          this.calculationStringOnScreen += input;
+          console.log(this.calculationString);
+
+          if(input == '+' || input == '-' || input == '*' || input == '/'){
+              this.calculationStringOnScreen = '';
+          }
+      },
+      calculate(){
+          var calculation = eval(this.calculationString);
+          this.calculation = calculation;
+          this.calculationStringOnScreen = '';
+      },
+      clear(){
+          this.calculation = 0;
+          this.calculationString = '';
+          this.calculationStringOnScreen = '';
+      }
+  }
+}).mount('#app')
